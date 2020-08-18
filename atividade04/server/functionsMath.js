1326b51211dbdde39c69f1ebfbd34aaa36cbd809
Lucas M. Rodrigues <lukas.mr@hotmail.com>
2020-08-18 17:25:54 -0300

    Finalizado a tarefa conforme solicitado, ainda para melhorar e talvez até criar uma calculadora mesmo

--- atividade04/server/functionsMath.js
+++ atividade04/server/functionsMath.js
@@ -1,21 +1,21 @@
 function add(n1, n2) {
     const result = n1 + n2;
-    return result;
+    return "O valor da Soma é : " + result;
 }
 
 function sub(n1, n2) {
     const result = n1 - n2;
-    return result;
+    return "O valor da Subtração é : " + result;
 }
 
 function multi(n1, n2) {
     const result = n1 * n2;
-    return result;
+    return "O valor da Multiplicação é : " + result;
 }
 
 function div(n1, n2) {
     const result = n1 / n2;
-    return result;
+    return "O valor da Divisão é : " + result;
 }
 
 module.exports = {
